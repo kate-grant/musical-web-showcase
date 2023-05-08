bdc29db5c8c0e6458bb266271f404360e58070f6
Glitch (glitch-hello-website) <none>
2023-05-08 02:01:11 +0000

    🚴✋ Checkpoint ./script.js:19393031/392 ./index.html:19393031/42

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+console.log(THREE);
 
 var WIDTH = window.innerWidth,
         HEIGHT = window.innerHeight,
