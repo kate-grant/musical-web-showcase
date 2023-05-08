9cb03246814392d943cad86cf116230f23974a42
Glitch (glitch-hello-website) <none>
2023-05-08 15:21:13 +0000

    👔🚵 Checkpoint ./script.js:19393031/98 ./style.css:19393031/167 ./sp-code.js:19393031/4

--- sp-code.js
+++ sp-code.js
@@ -8,7 +8,7 @@ export function spCode() {
 
     let scale = input(1., 2., 10.);
     let scale2 = input(1., 1., 100.);
-    let n = noise(s*audio*0.2 + vec3(0,0,time)) + noise(r*.4 + vec3(audio,audio*0.1,time*audio*0.7));
+    let n = noise(s*audio*0.1 + vec3(0,0,time)) + noise(r*.4 + vec3(audio,audio*0.1,time*audio*0.9));
     color(normal * .3 + vec3(8, 3, 1));
     setStepSize(0.3);
     sphere(0.8+0.2*n);`
