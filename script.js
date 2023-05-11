8f32825c25ce4740ba6680534ff7a9ace43268ae
Glitch (glitch-hello-website) <none>
2023-05-11 09:34:09 +0000

    📷🎃 Checkpoint ./.glitch-assets:19393031/1588 ./script.js:19393031/576

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ audioLoader.load( 'https://cdn.glitch.global/9b48c83c-6c8e-4281-a381-d57318641fc
       state.currAudio = 0.0;
     } else {
       sound.play();
-      button.style.backgroundImage = "url(https://cdn.glitch.global/9b48c83c-6c8e-4281-a381-d57318641fca/btn_1_pause_i.png?v=1683796726665)";
+      button.style.backgroundImage = "url(https://cdn.glitch.global/9b48c83c-6c8e-4281-a381-d57318641fca/btn_1_pause_l.png?v=1683797411557)";
       // button.innerHTML = 'STOP';
       button.alt = 'pause';
     }
